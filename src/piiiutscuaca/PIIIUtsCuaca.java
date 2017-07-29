fca5996c356890346794ed9ea4ba96cc8190443e
Iman Muliawan <imanmuliawan@gmail.com>
2017-07-29 07:31:27 +0700

    PIIIUTS - upload1

---
+++ src/piiiutscuaca/PIIIUtsCuaca.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piiiutscuaca;
+
+import piiiutscuaca.view.FrmCuaca;
+
+/**
+ * main class
+ * @author imanmuliawan
+ */
+public class PIIIUtsCuaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        FrmCuaca frmCuaca = new FrmCuaca();
+        frmCuaca.setLocationRelativeTo(null);
+        frmCuaca.setVisible(true);
+    }
+    
+}
